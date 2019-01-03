0c13f78539324bfcf6fa51d7ec666af059e07e2e
yimchedong <yimchedong@eosdaq.com>
2019-01-03 16:08:09 +0900

    implement iframe parent window code

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var iframeDomain = 'http://eosdaq.test:3000';
+
 const network = {
   blockchain: "eos",
   chainId: "8be32650b763690b95b7d7e32d7637757a0a7392ad04f1c393872e525a2ce82b",
@@ -10,7 +12,60 @@ const network = {
 const signin = document.getElementById('scatter-signin')
 const signout = document.getElementById('scatter-signout')
 const result = ScatterJS.scatter.connect('Persian Pink')
-const iframe = document.getElementById('eosdaq').contentWindow
+const iframe = document.getElementById('eosdaq').contentWindow;
+
+function Eosdaq(scatter) {
+  this.scatter = scatter;
+
+  var ___messageToEosdaq = (action, payload) => {
+    var message = {
+      action: action,
+      payload: payload || '',
+    };
+  
+    iframe.postMessage(JSON.stringify(message), iframeDomain);
+  }
+
+  this.login = (payload) => {
+    __messageToEosdaq('getIdentity', payload);
+  };
+
+  this.logout = () => {
+    __messageToEosdaq('forgetIdentity');
+  };
+
+  this.transfer = (data) => {
+    this.scatter.eos.transaction(data).then(() => {
+      ___messageToEosdaq('transferResult', {
+        success: true,
+      });
+    }).catch((err) => {
+      ___messageToEosdaq('transferResult', {
+        success: true,
+        err: err
+      });
+    })
+  }
+
+  function onMessage(e) {
+    if (e.origin !== iframeDomain) {
+      return;
+    }
+    
+    var data = e.data;
+    if (data.action === 'transfer') {
+      this.transfer(JSON.parse(data));
+    }
+  }
+
+  window.addEventListener('message', onMessage);
+  
+  this.destory = function() {
+    window.removeEventListener('message', onMessage);
+  }
+}
+
+window.eosdaq = new Eosdaq(window.scatter);
 
 signin.onmousedown = async (e) => {
   e.preventDefault()
@@ -32,6 +87,10 @@ signout.onmousedown = (e) => {
     action: 'forgetIdentity'
   })
 }
+
+
+
+
 
 // window.onmessage = (e) => {
 //   e.origin === 'iframeDomain'
